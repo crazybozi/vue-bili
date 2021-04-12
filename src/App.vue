26c992903971fe8cfb5ccbbddda382b729a5670d
ffz <2693281866@qq.com>
2021-04-12 16:50:21 +0800

    添加个人信息,推送到远程

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+ <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-row>
+      <el-button>这里应该是bili的icon图标</el-button>
+      <el-button type="primary">主站</el-button>
+      <el-button type="success">番剧</el-button>
+      <el-button type="info">游戏中心</el-button>
+      <el-button type="warning">直播</el-button>
+      <el-button type="danger">会员购</el-button>
+      <el-button type="danger">漫画</el-button>
+      <el-button type="danger">赛事</el-button>
+      <el-button type="danger">下载APP</el-button>
+    </el-row>
   </div>
 </template>
 
